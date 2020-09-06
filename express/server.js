490f1560678f48ddcd4a0b5517b57738a26887db
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2020-09-06 17:36:22 +0200

    eliminate unnecessary requests

--- express/server.js
+++ express/server.js
@@ -1,139 +1,138 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 require('encoding');
 
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-async function retrieveNotionData(entry){
-    const masterId = transformStringIntoId(entry);
+// async function retrieveNotionData(entry){
+//     const masterId = transformStringIntoId(entry);
 
-    function transformStringIntoId(str) {
-        const subStr1 = str.substring(0, 8);
-        const subStr2 = str.substring(8, 12);
-        const subStr3 = str.substring(12, 16);
-        const subStr4 = str.substring(16, 20);
-        const subStr5 = str.substring(20);
-        return `${subStr1}-${subStr2}-${subStr3}-${subStr4}-${subStr5}`
-    }
+//     function transformStringIntoId(str) {
+//         const subStr1 = str.substring(0, 8);
+//         const subStr2 = str.substring(8, 12);
+//         const subStr3 = str.substring(12, 16);
+//         const subStr4 = str.substring(16, 20);
+//         const subStr5 = str.substring(20);
+//         return `${subStr1}-${subStr2}-${subStr3}-${subStr4}-${subStr5}`
+//     }
 
-    async function requestData(id) {
-        const options = {
-            headers: {
-                'content-type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                'pageId': id,
-                'limit':50,
-                'cursor':{
-                    'stack':[[{
-                        'table':'block','id':id,'index':0
-                    }]]},
-                'chunkNumber':0,
-                'verticalColumns':false
-            })
-        }
+//     async function requestData(id) {
+//         const options = {
+//             headers: {
+//                 'content-type': 'application/json'
+//             },
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 'pageId': id,
+//                 'limit':50,
+//                 'cursor':{
+//                     'stack':[[{
+//                         'table':'block','id':id,'index':0
+//                     }]]},
+//                 'chunkNumber':0,
+//                 'verticalColumns':false
+//             })
+//         }
         
-        try {
-            const response = await fetch('https://www.notion.so/api/v3/loadPageChunk', options);
-            const resJson = await response.json();
-            return resJson
-        } catch(error) {
-            console.log(error);
-        }
-    }
+//         try {
+//             const response = await fetch('https://www.notion.so/api/v3/loadPageChunk', options);
+//             const resJson = await response.json();
+//             return resJson
+//         } catch(error) {
+//             console.log(error);
+//         }
+//     }
 
-    function transformEntryPage(data){
-        const blog = {
-            entryPage: {
-                title: null,
-                posts: []
-            },
-            posts: []
-        }
-        const { recordMap : { block } } = data;
-        const blockKeys = Object.keys(block)
-        blockKeys.forEach((key, i)  => {
-            const entry = block[key];
-            const content = entry.value.properties && entry.value.properties.title[0][0];
-            const created = entry.value && entry.value.created_time;
-            const edited = entry.value && entry.value.last_edited_time;
-            if (i == 0) {
-                blog.entryPage.title = content;
-            } else {
-                blog.entryPage.posts.push({
-                    id: key, 
-                    postTitle: content,
-                    created,
-                    edited
-                })
-            }
-        });
-        return blog;
-    }
+//     function transformEntryPage(data){
+//         const blog = {
+//             entryPage: {
+//                 title: null,
+//                 posts: []
+//             },
+//             posts: []
+//         }
+//         const { recordMap : { block } } = data;
+//         const blockKeys = Object.keys(block)
+//         blockKeys.forEach((key, i)  => {
+//             const entry = block[key];
+//             const content = entry.value.properties && entry.value.properties.title[0][0];
+//             const created = entry.value && entry.value.created_time;
+//             const edited = entry.value && entry.value.last_edited_time;
+//             if (i == 0) {
+//                 blog.entryPage.title = content;
+//             } else {
+//                 blog.entryPage.posts.push({
+//                     id: key, 
+//                     postTitle: content,
+//                     created,
+//                     edited
+//                 })
+//             }
+//         });
+//         return blog;
+//     }
 
-    async function getRowDataForPosts(data) {
-        const { entryPage: { posts}  } = data;
-        const postsRowData = await Promise.all(posts.map(async ({id}) => ({id, data: await requestData(id)})));
-        postsRowData.forEach(postData => {
-            const matchingPost = posts.find(({id}) => postData.id === id)
-            if(matchingPost) {
-                matchingPost.postRowData = postData;
-            }
-        });
-        return {...data, posts};
-    }
+//     async function getRowDataForPosts(data) {
+//         const { entryPage: { posts}  } = data;
+//         const postsRowData = await Promise.all(posts.map(async ({id}) => ({id, data: await requestData(id)})));
+//         postsRowData.forEach(postData => {
+//             const matchingPost = posts.find(({id}) => postData.id === id)
+//             if(matchingPost) {
+//                 matchingPost.postRowData = postData;
+//             }
+//         });
+//         return {...data, posts};
+//     }
 
-    function getTextContentFromData(data) {
-        const { posts } = data;
-        const parsedPosts = posts
-        .map(({ postRowData, id }) => {
-            if (postRowData && postRowData.data) {
-                return {
-                    title: postRowData.data.recordMap.block[id].value.properties.title[0][0],
-                    [id] : Object.keys(postRowData.data.recordMap.block)
-                    .filter(key => key === id || key === masterId ? null : key)
-                    .filter(key => postRowData.data && postRowData.data.recordMap.block[key].value.properties)
-                    .map((key, index) => ({
-                        text: postRowData.data && postRowData.data.recordMap.block[key].value.properties.title[0][0],
-                        type: postRowData.data && postRowData.data.recordMap.block[key].value.type,
-                        position: index
-                    }))
-                }
-            }
-        });
-        return {...data, posts: parsedPosts};
-    }
+//     function getTextContentFromData(data) {
+//         const { posts } = data;
+//         const parsedPosts = posts
+//         .map(({ postRowData, id }) => {
+//             if (postRowData && postRowData.data) {
+//                 return {
+//                     title: postRowData.data.recordMap.block[id].value.properties.title[0][0],
+//                     [id] : Object.keys(postRowData.data.recordMap.block)
+//                     .filter(key => key === id || key === masterId ? null : key)
+//                     .filter(key => postRowData.data && postRowData.data.recordMap.block[key].value.properties)
+//                     .map((key, index) => ({
+//                         text: postRowData.data && postRowData.data.recordMap.block[key].value.properties.title[0][0],
+//                         type: postRowData.data && postRowData.data.recordMap.block[key].value.type,
+//                         position: index
+//                     }))
+//                 }
+//             }
+//         });
+//         return {...data, posts: parsedPosts};
+//     }
 
-    const data = await requestData(masterId);
-    if (data) {
-        const blogEntryPage = transformEntryPage(data);
-        const postsWithRowData = await getRowDataForPosts(blogEntryPage);
-        const transformedPosts = getTextContentFromData(postsWithRowData)
-        return transformedPosts; 
-    }
+//     const data = await requestData(masterId);
+//     if (data) {
+//         const blogEntryPage = transformEntryPage(data);
+//         const postsWithRowData = await getRowDataForPosts(blogEntryPage);
+//         const transformedPosts = getTextContentFromData(postsWithRowData)
+//         return transformedPosts; 
+//     }
+// }
+
+// const router = express.Router();
+
+function updateDatabase(data) {
+  const newValue = {'data': data}
+  return newValue;
 }
+app.use(bodyParser);
+app.get('/:entry', (res, req) => {
+  const newValue = updateDatabase(req.params.entry)
+  res.json(newValue);
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-
-router.get('/:entry', (req, res) => {
-  retrieveNotionData(req.params.entry).then(data => res.json(data));
 })
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/.netlify/functions/server');  // path must route to lambda
 
 module.exports = app;
+
 module.exports.handler = serverless(app);
